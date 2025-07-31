4a3750358c57cda9f3a4ae2b35352d61486cf8c9
shohan12727 <aishohan001@gmail.com>
2025-08-01 01:05:48 +0600

    updated

---
+++ tapascript/lexical.js
@@ -0,0 +1,27 @@
+function outer () {
+  const name = "Shohan";
+
+  function inner () {
+    console.log(name);
+  }
+  inner ();
+}
+outer();
+
+
+const userName = 'Jhon';
+console.log(userName)
+
+const nums = [1,4,5,2,5];
+console.log(nums);
+nums[2] = 50;
+console.log(nums);
+
+
+
+const myName = 'Ashraful Islam Shohan';
+for(let i = myName.length - 1; i > 0; i--){
+  console.log(myName[i]); 
+}
+
+
