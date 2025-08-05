d879b65aa5f831bc9ecb52bee352419e24aa8833
shohan12727 <aishohan001@gmail.com>
2025-08-05 23:22:35 +0600

    client side

--- mongo-client/src/App.jsx
+++ mongo-client/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
-
+import Users from './Components/Users'
 function App() {
  
 
   return (
     <>
     <h1>Simple CURD Operation</h1>
+    <Users></Users>
     </>
   )
 }
--- mongo-client/src/Components/Users.jsx
+++ mongo-client/src/Components/Users.jsx
@@ -1,10 +1,48 @@
 import React from 'react';
 
 const Users = () => {
+
+
+  const handleUser = e => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const newUser = { name, email };
+    console.log(newUser);
+    // create user in the database
+    fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log('data after creating user in the database', data);
+
+      })
+
+
+  }
+
+
+
+
   return (
     <div>
-      
+      {/* add user */}
+      <div>
+        <form onSubmit={handleUser}>
+          <input type="text" name='name' />
+          <br />
+          <input type="email" name="email" id="" />
+          <br />
+          <input type="submit" value="Add User" />
+        </form>
+      </div>
     </div>
+
   );
 };
 
