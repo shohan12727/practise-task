4c67efb946a0d5e672d8a2c9fff88979d24d012e
shohan12727 <aishohan001@gmail.com>
2025-08-15 13:59:06 +0600

    fix problem

--- Assignment-4/problem5.js
+++ Assignment-4/problem5.js
@@ -1,28 +1,34 @@
-function  resultReport( marks ) {
-          if(Array.isArray(marks) !== true){
-            return 'Invalid';
-          }
-          let sum = 0;
-          let passCount = 0;
-          let failCount = 0;
-      
-      for(let i = 0 ; i < marks.length ; i ++){
-        sum += marks[i];
-        
-        if(marks[i] >= 40){
-          passCount ++;
-        } else {
-          failCount ++;
-        }
-      }  
-      
-      const avg = Math.round(sum / marks.length);
-      return {
-        finalScore: avg,
-        pass: passCount,
-        fail: failCount
-      }
+function resultReport(marks) {
+  if (Array.isArray(marks) !== true) {
+    return 'Invalid';
+  }
+  let sum = 0;
+  let passCount = 0;
+  let failCount = 0;
+
+  for (let i = 0; i < marks.length; i++) {
+    sum += marks[i];
+
+    if (marks[i] >= 40) {
+      passCount++;
+    } else {
+      failCount++;
+    }
+  }
+
+
+  let avg = Math.round(sum / marks.length);
+
+  if (isNaN(avg)) {
+    avg = 0;
+  }
+  return {
+    finalScore: avg,
+    pass: passCount,
+    fail: failCount
+  }
+
 }
 
 
-console.log(resultReport(100));
+console.log(resultReport([]));
