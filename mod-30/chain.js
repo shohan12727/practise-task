186f1fb5d00623890e4d7ec398aea429f7f55cab
shohan12727 <aishohan001@gmail.com>
2025-09-01 00:47:48 +0600

    optional chain

---
+++ mod-30/chain.js
@@ -0,0 +1,14 @@
+const student = {
+  name: "pal",
+  marks: 90,
+  1: 50,
+  family: {
+    title: 'legend',
+    // mother: {
+    //   name: 'noor jahan'
+    // }
+  },
+  'home_address': 'Boalmari'
+}
+console.log(student.family.mother?.name);
+
