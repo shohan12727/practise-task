f04dab914665890afbde11e5b9c84b42e3d5deca
shohan12727 <aishohan001@gmail.com>
2025-09-10 21:48:23 +0600

    async practise

---
+++ async/async.js
@@ -0,0 +1,15 @@
+console.log("One");
+console.log("two");
+three();
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+console.log("four");
+
+function three() {
+  console.log("three");
+}
+
+setTimeout(three, 5000)
+
+setInterval(three,1000)
