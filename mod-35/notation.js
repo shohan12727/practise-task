9aaf751b526a33ea9a185a2fab30301875c54ab8
shohan12727 <aishohan001@gmail.com>
2025-09-16 00:54:01 +0600

    braket notation practise

---
+++ mod-35/notation.js
@@ -0,0 +1,16 @@
+const person = {
+  firstName: "Shohan",
+  22: "Islam",
+  age: 20,
+  hobbies: ["coding", "reading", "gaming"],
+  address: {
+    city: "Dhaka",
+    country: "Bangladesh"
+  },
+  "favorite color": "blue"
+};
+
+console.log(person["firstName"]);
+console.log(person["favorite color"]);
+
+console.log(person[22]);
